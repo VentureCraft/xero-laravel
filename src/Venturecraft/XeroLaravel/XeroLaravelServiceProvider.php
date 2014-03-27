da861848032188a443dd72a137c870c602040e6a
Chris Duell <me@chrisduell.com>
2014-03-27 19:29:19 +1100

    initial commit

---
+++ src/Venturecraft/XeroLaravel/XeroLaravelServiceProvider.php
@@ -0,0 +1,39 @@
+<?php namespace Venturecraft\XeroLaravel;
+
+use Illuminate\Support\ServiceProvider;
+
+class XeroLaravelServiceProvider extends ServiceProvider {
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app['xero'] = $this->app->share(function($app)
+        {
+            \ClassLoader::addDirectories(array(__DIR__ . '/PHP-Xero'));
+            $config = $this->app['config']['xero'];
+            return new \xero($config['key'], $config['secret'], $config['publicPath'], $config['privatePath'], $config['format']);
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array('xero');
+    }
+
+}
